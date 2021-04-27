d513808efcf1d438a84dd53c1be14971af0af6e9
Jordan Long <jblong27@gmail.com>
2021-04-27 12:36:41 -0500

    server changes

--- server.js
+++ server.js
@@ -58,6 +58,17 @@ app.get('/lyrics', async (req, res) => {
   res.json({ lyrics })
 })
 
+if (process.env.NODE_ENV === 'production') {
+  // Exprees will serve up production assets
+  app.use(express.static('client/build'));
+
+  // Express serve up index.html file if it doesn't recognize route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 app.listen(port, function() {
   console.log('App running on port 3001');
 })
